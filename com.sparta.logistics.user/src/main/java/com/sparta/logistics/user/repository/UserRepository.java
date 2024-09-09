de580bb60e4a2b70c0f723dc0d9e14d555bd9ddf
dnjawm19 <dnjawm19@naver.com>
2024-09-09 11:40:35 +0900

    refactor: UserRepository 누락된 코드 추가

--- com.sparta.logistics.user/src/main/java/com/sparta/logistics/user/repository/UserRepository.java
+++ com.sparta.logistics.user/src/main/java/com/sparta/logistics/user/repository/UserRepository.java
@@ -3,6 +3,11 @@ package com.sparta.logistics.user.repository;
 import com.sparta.logistics.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String name);
+
+    Optional<User> findByEmail(String email);
 }
