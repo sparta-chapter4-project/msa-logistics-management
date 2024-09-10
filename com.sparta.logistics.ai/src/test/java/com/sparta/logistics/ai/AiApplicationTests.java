674dba2875d8f516956c197a7f397ba63bec6670
dnjawm19 <dnjawm19@naver.com>
2024-09-11 02:25:33 +0900

    feat: first init ai

---
+++ com.sparta.logistics.ai/src/test/java/com/sparta/logistics/ai/AiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.logistics.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
