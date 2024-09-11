6f2f0e2bf01f1960e10d97ca8f5ab6e0ec376893
kyeonkim <kyeonkim5021@gmail.com>
2024-09-11 12:12:29 +0900

    [Feat] Init Delivery Manager #40

---
+++ com.sparta.logistics.deliverymanager/src/main/java/com/sparta/logistics/deliverymanager/DeliverymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.logistics.deliverymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverymanagerApplication.class, args);
+	}
+
+}
---
+++ com.sparta.logistics.deliverymanager/src/test/java/com/sparta/logistics/deliverymanager/DeliverymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.logistics.deliverymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
